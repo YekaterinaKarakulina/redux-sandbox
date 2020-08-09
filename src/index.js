3fb4f5ceb03db94c6e46addc33571ad7c9f76f34
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-08-09 09:45:01 +0400

    feat: add bindActionCreators()

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
-import { createStore } from 'redux';
+import { createStore, bindActionCreators } from 'redux';
 import reducer from './reducer';
-import { inc, dec, rnd } from './actions';
+/* import { inc, dec, rnd } from './actions'; */
+import * as actions from './actions';
 
 const store = createStore(reducer);
+const { dispatch } = store;
 
-document.getElementById('inc').addEventListener('click', () => {
-  store.dispatch(inc());
-});
+/* const bindActionCreator = (creator, dispatch) => (...args) => {
+  dispatch(creator(...args));
+} */
 
-document.getElementById('dec').addEventListener('click', () => {
-  store.dispatch(dec());
-});
+/* const incDispatch = bindActionCreators(inc, dispatch)
+const decDispatch = bindActionCreators(dec, dispatch)
+const rndDispatch = bindActionCreators(rnd, dispatch) */
 
-document.getElementById('rnd').addEventListener('click', () => {
-  const payload = Math.floor(Math.random() * 10);
-  store.dispatch(rnd(payload));
-});
+/* const { incDispatch, decDispatch, rndDispatch } = bindActionCreators({
+  incDispatch: inc,
+  decDispatch: dec,
+  rndDispatch: rnd
+}, dispatch) */
+
+const { inc, dec, rnd } =
+  bindActionCreators(actions, dispatch)
+
+document
+  .getElementById('inc')
+  .addEventListener('click', inc);
+
+document
+  .getElementById('dec')
+  .addEventListener('click', dec);
+
+document
+  .getElementById('rnd')
+  .addEventListener('click', () => {
+    const payload = Math.floor(Math.random() * 10);
+    rnd(payload);
+  });
 
 const update = () => {
   document.getElementById('counter').innerHTML = store.getState();
